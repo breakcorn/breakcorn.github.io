c89e45bd4ac9ec142e70e32df19c951cb2415ee7
breakcorn <breakcorny@gmail.com>
2024-07-03 22:14:32 +0300

    0.2.1

---
+++ save.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+IFS='.' read -r -a array <<< "$(cat version)"
+array[2]="$((array[2] + 1))"
+version="${array[0]}.${array[1]}.${array[2]}"
+echo version > version
+
+git config --global user.name "breakcorn"
+git config --global user.email "breakcorny@gmail.com"
+
+git add .
+git status
+git commit -m "${version}"
+git branch -M main
+git push -u origin main
