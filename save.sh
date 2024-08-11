95a5c8794f8cc517a1827b6402ec2dea9c4e54bf
breakcorn <breakcorny@gmail.com>
2024-08-12 01:51:05 +0300

    0.2.10

--- save.sh
+++ save.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-set -e
+# https://itisgood.ru/2024/03/13/chto-takoe-ifs-v-skriptakh-na-bash/
 
-IFS='.' read -r -a array <<< "$(cat version)"
-array[2]="$((array[2] + 1))"
-version="${array[0]}.${array[1]}.${array[2]}"
-echo $version
-# exit 0
-echo $version > ./version
+set -e
 
 git config --global user.name "breakcorn"
 git config --global user.email "breakcorny@gmail.com"
 
-git add .
-git status
-git commit -m "${version}"
+IFS='.' read -r -a array <<< "$(cat version)"
+
+if git diff-index --quiet HEAD --; then
+  echo "There are NO changes to the working directory."
+  VERSION="${array[0]}.${array[1]}.${array[2]}"
+  echo "Current version $VERSION. Remains unchanged."
+  # exit 0
+  if [ -n "$(git log origin/$(git branch --show-current)..HEAD)" ]; then
+    echo "There are unsent commits."
+    git status
+  else
+    echo "There are NO uncommitted commits."
+    git status
+    exit 0
+  fi
+else
+  echo "There are changes in the working directory."
+  VERSION="${array[0]}.${array[1]}.$((array[2] + 1))"
+  echo "The current version has been changed to $VERSION."
+  # exit 0
+  echo $VERSION > ./version
+  git add .
+  git status
+  git commit -m "${VERSION}"
+fi
+
 git branch -M main
 git push -u origin main
